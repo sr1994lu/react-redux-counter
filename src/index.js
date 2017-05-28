2d178de6e9640333e8dceb164e97bfd1edba3de1
Shunsuke NISHINO <sr1994lu@gmail.com>
2017-05-28 22:40:52 +0900

    refactor(core): update

--- src/index.js
+++ src/index.js
@@ -2,47 +2,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
+import Counter from './components/Counter';
+import counter from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
-const counter = (state = 0, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1;
-    case 'DECREMENT':
-      return state - 1;
-    default:
-      return state;
-  }
-};
-
-const Counter = ({
-  value,
-  onIncrement,
-  onDecrement,
-}) => (
-    <div>
-      <h1>{value}</h1>
-      <button onClick={onIncrement}>+</button>
-      <button onClick={onDecrement}>-</button>
-    </div>
-);
-
 const store = createStore(counter);
+const rootEl = document.getElementById('root');
 
-const render = () => {
-  ReactDOM.render(
-      <Counter
-          value={store.getState()}
-          onIncrement={() => store.dispatch({
-            type: 'INCREMENT',
-          })}
-          onDecrement={() => store.dispatch({
-            type: 'DECREMENT',
-          })}
-      />,
-      document.getElementById('root'));
-};
-
-store.subscribe(render);
+const render = () => ReactDOM.render(
+    <Counter
+        value={store.getState()}
+        onIncrement={() => {
+          store.dispatch({type: 'INCREMENT'});
+        }}
+        onDecrement={() => {
+          store.dispatch({type: 'DECREMENT'});
+        }}
+    />,
+    rootEl,
+);
 render();
+store.subscribe(render);
 registerServiceWorker();
